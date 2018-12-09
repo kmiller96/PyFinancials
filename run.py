bc4c6dd6815a4ba277dcb33542501522d862552c
kmiller96 <kale.miller96@gmail.com>
2018-12-09 21:34:37 +1100

    Created run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+"""
+The main execution of the module.
+
+Useful for testing that the model was built correctly and to get an idea of
+example functionality. There are (read: soon will be) multiple user stories
+built as individual execution runs so you can see some example code in action.
+"""
+
+import pyfinancials as pf
